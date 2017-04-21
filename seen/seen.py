652d37f103892eebb39605340d36d17ad67aec76
Ryonez <Ryonez@users.noreply.github.com>
2017-04-21 12:48:46 +1200

    Commit patch note error tracking #11

--- seen/seen.py
+++ seen/seen.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from cogs.utils.dataIO import dataIO
 import discord
 import os
-#Commit patch note error tracking #10
+#Commit patch note error tracking #11
 
 class Seen:
     '''Check when someone was last seen.'''
