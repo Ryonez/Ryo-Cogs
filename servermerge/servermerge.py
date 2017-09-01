cc7ede5110596045af129a43d4e9ee242ea578f1
Ryonez <Ryonez@users.noreply.github.com>
2017-09-02 02:20:53 +1200

    Name issue in serverstatus
    Spelling, yay.

--- servermerge/servermerge.py
+++ servermerge/servermerge.py
@@ -131,7 +131,7 @@ class Servermerge:
                 eroleid = erole.id
 
             if statuschannel is None:
-                statchannelmention = "*Not set!"
+                statuschannelmention = "*Not set!"
             else:
                 statuschannelmention = statuschannel.mention
 
