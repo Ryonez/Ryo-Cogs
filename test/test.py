b3491680316e0fc7ed0fd23bf0d48ae182c3e5e8
Ryonez <Ryonez@users.noreply.github.com>
2018-09-15 09:18:55 +1200

    test

--- test/test.py
+++ test/test.py
@@ -13,18 +13,45 @@ class test:
 
     @commands.command()
     @checks.is_owner()
-    async def emtest(self):
+    async def emtest(self, ctx):
         """Test code"""
 
         timestamp = datetime.datetime.today()
 
-        embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format("test1", "test2"),
+        embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
                                         colour=discord.Colour(0x54d824),
                                         # description="This server is currently on `{}` of setup".format(stage),
                                         timestamp = timestamp)
 
         await self.bot.say(embed=embedmsg)
 
+    @commands.command()
+    @checks.is_owner()
+    async def emtest1(self, ctx):
+        """Test code"""
+
+        timestamp = datetime.datetime.today()
+
+        embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
+                                        colour=discord.Colour(0x54d824),
+                                        description="{} was given the {} role".format(ctx.message.author.name, ctx.message.author.top_role)
+                                        timestamp = timestamp)
+
+        await self.bot.say(embed=embedmsg)
+
+    @commands.command()
+    @checks.is_owner()
+    async def emtest2(self, ctx):
+        """Test code"""
+
+        timestamp = datetime.datetime.today()
+
+        embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
+                                        colour=discord.Colour(0x54d824),
+                                        # description="This server is currently on `{}` of setup".format(stage),
+                                        timestamp = timestamp)
+
+        await self.bot.say(embed=embedmsg)
 
 
 
