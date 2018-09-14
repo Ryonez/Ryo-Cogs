78cb184edb90c7c7dc23c0e5d1b45096647415c8
Ryonez <Ryonez@users.noreply.github.com>
2018-09-15 09:58:09 +1200

    test

--- test/test.py
+++ test/test.py
@@ -17,25 +17,31 @@ class test:
         """Test code"""
 
         timestamp = datetime.datetime.today()
+        auth = ctx.message.author
 
         embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
                                         colour=discord.Colour(0x54d824),
                                         # description="This server is currently on `{}` of setup".format(stage),
                                         timestamp = timestamp)
+        embedmsg.add_footer(text = "{}'s (Greeter) ID: `{}` | {}'s (New member) ID `{}`".format(auth.name, auth.id, auth.mention, auth.id))
 
         await self.bot.say(embed=embedmsg)
 
     @commands.command(pass_context=True)
-    @checks.is_owner()
+    @checks.admin_or_permissions(administrator=True)
     async def emtest1(self, ctx):
         """Test code"""
 
         timestamp = datetime.datetime.today()
+        auth = ctx.message.author
 
-        embedmsg = discord.Embed(title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
+        embedmsg = discord.Embed(#title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
                                         colour=discord.Colour(0x54d824),
                                         description="{} was given the {} role".format(ctx.message.author.name, ctx.message.author.top_role),
                                         timestamp = timestamp)
+        embedmsg.set_author(name="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name,
+                            icon_url=auth.avatar_url)
+        embedmsg.add_footer(text = "{}'s (Greeter) ID: `{}` | {}'s (New member) ID `{}`".format(auth.name, auth.id, auth.mention, auth.id)
 
         await self.bot.say(embed=embedmsg)
 
