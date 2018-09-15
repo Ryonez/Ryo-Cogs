21b12e495180947a6eced923bf2a314a48cd3a60
Ryonez <Ryonez@users.noreply.github.com>
2018-09-15 19:36:19 +1200

    Fullt initial greeter release.

--- greeter/greeter.py
+++ greeter/greeter.py
@@ -5,13 +5,16 @@ from .utils import checks
 import datetime
 import discord
 import os
+import logging
 from collections import defaultdict
+import asyncio
 
 server_template = {
     "greeterroleid" : None,
     "memberroleid" : None,
     "greetlogchid" : None,
-    "greetmsg" : None,
+    "greetchannelid" : None,
+    "removetriggercmd" : False,
     "enabled" : False
 }
 
@@ -19,66 +22,292 @@ class Greeter:
 
     def __init__(self, bot):
         self.bot = bot
-        setpath = os.path.join('data', 'greet', 'settings.json')
+        setpath = os.path.join('data', 'greeter', 'settings.json')
         settings = dataIO.load_json(setpath)
         self.settings = defaultdict(lambda: server_template.copy(), settings)
 
-    @commands.group(pass_context=True, no_pm=True)
+    @commands.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
     async def greet(self, ctx, user: discord.User):
-        """The greeter command, passed a role to greet someone into the server."""
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
-            return
+        """The greeter command, pass a role to greet someone into the server."""
 
         server = ctx.message.server
+        author = ctx.message.author
 
         if self.settings[server.id].get("enabled"):
             
-            mrole = discord.utils.get(server.roles, id=self.settings[server.id].get("mrolesid"))
+            grole = discord.utils.get(server.roles, id=self.settings[server.id].get("greeterroleid"))
+            mrole = discord.utils.get(server.roles, id=self.settings[server.id].get("memberroleid"))
+            greetch = discord.utils.get(server.channels, id=self.settings[server.id].get("greetchannelid"))
+            clrcmd = self.settings[server.id].get("removetriggercmd")
 
-            if ctx.message.author is self._isgreeter_(server, ctx.author):
-                timestamp = datetime.datetime.today()
-                try:
-                    await self.bot.add_roles(user, mrole)
-                    await self._log_("Success", timestamp, server, ctx.author, user)
-                except discord.Forbidden:
-                    await self._log_("Forbidden", timestamp, server)
-                    return
+            if greetch is not None and ctx.message.channel is not greetch:
+                return
+
+            if self._hasrole_(author, grole):
+                timestamp = ctx.message.timestamp
+                
+                if self._hasrole_(user, mrole):
+                    wtfemoji = discord.utils.get(self.bot.get_all_emojis(), id="330424102915407872").url
+                    embed = discord.Embed()
+                    embed.colour = discord.Colour(0x0F71B5)
+                    embed.description = "What you playing at {}?".format(author.mention)
+                    name = "{} is already a member!".format(user.name)
+                    embed.set_thumbnail(url = wtfemoji)
+                    embed.set_author(name=name)
+                    embed.set_footer(text = "This message will self delete in 30 seconds")
+                    msg = await self.bot.say(embed = embed)
+                    await asyncio.sleep(30)
+                    await self.bot.delete_message(msg)
+                else:
+                    try:
+                        await self.bot.add_roles(user, mrole)
+                        await self._greetlogger_("Success", timestamp, server, author, user, mrole)
+                    except discord.Forbidden:
+                        await self._greetlogger_("Forbidden", timestamp, server, author, user, mrole)
+
+                if clrcmd:
+                    await asyncio.sleep(30)
+                    await self.bot.delete_message(ctx.message)
+
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(administrator=True)
+    async def greetset(self, ctx):
+        """Settings for greeter. Having the greeter role, member role and log channel is required for greet to work fully."""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @greetset.command(name="grole", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _grole_(self, ctx, role: discord.Role):
+        """Sets the greeter role for the server. You can either mention a role, or give me a role ID.\n [p]greetset mrole @user"""
+
+        server = ctx.message.server
+
+        self.settings[server.id]["greeterroleid"] = role.id
+        self.save()
+        embed = discord.Embed()
+        embed.colour = discord.Colour.green()
+        embed.description = "{} has been saved as the greeter role. Please note users with this role will be able to add the specified member role even if they don't have the perms to when greeter is enabled. Yo've been warned.".format(role.mention)
+        name = "The greeter role was succesfully set."
+        embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+        await self.bot.say(embed = embed)
 
 
+    @greetset.command(name="mrole", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _mrole_(self, ctx, role: discord.Role):
+        """Sets the member role for the server. You can either mention a role, or give me a role ID.\n [p]greetset mrole @user"""
 
-    def _isgreeter_(self, server, user):
-        # Check if the given user has the greeter role.
-        groleid = self.settings[server.id].get("greeterrole")
+        server = ctx.message.server
 
-        if groleid is None:
-            return False
+        self.settings[server.id]["memberroleid"] = role.id
+        self.save()
+        embed = discord.Embed()
+        embed.colour = discord.Colour.green()
+        embed.description = "{} has been saved as the member role. Please note users with the greeter role will be able to add the this role to others when greeter is enabled.".format(role.mention)
+        name = "The member role was succesfully set."
+        embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+        await self.bot.say(embed = embed)
+
+    @greetset.command(name="cleanupcmd", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _cleanupcmd_(self, ctx):
+        """Toggles weather the bot removes successfully triggered greet commands from greeters. Delays 30 seconds before doing so. \n [p]greetset cleanupcmd"""
+
+        server = ctx.message.server
+        clrcmd = self.settings[server.id].get("removetriggercmd")
+        
+        onemoji = discord.utils.get(self.bot.get_all_emojis(), id="330419505589256192").url
+        offemoji = discord.utils.get(self.bot.get_all_emojis(), id="330419505563959296").url
+
+        embed = discord.Embed()
+        embed.colour = discord.Colour.green()
+        
+        if clrcmd is True:
+            clrcmd = False
+            embed.description = "Successfully triggered greet commands will be deleted."
+            embed.set_author(name="Greet command cleanup has been toggled on.", icon_url=onemoji)
+            embed.colour = discord.Colour.green()
         else:
-            for r in user.roles:
-                if r.id == groleid:
-                    return True
-            return False
+            clrcmd = True
+            embed.description = "Successfully triggered greet commands will not be deleted."
+            embed.set_author(name="Greet command cleanup has been toggled off.", icon_url=offemoji)
+            embed.colour = discord.Colour(0xEA2011)
 
-    def _log_(self, status, timestamp, server, greetuser = None, newuser = None):
-        # Loggint for the greeter cog.
+        self.settings[server.id]["removetriggercmd"] = clrcmd
+        self.save()
+
+        await self.bot.say(embed = embed)
+
+    @greetset.command(name="loggerchannel", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _logggerchannel_(self, ctx, channel: discord.Channel = None):
+        """Sets the channel greeter will log to. Mention a channel with this command, or use it's ID to set it.\ni.e: [p]greetset loggerchannel #greeter-logs\n"""
+
+        server = ctx.message.server
+        
+        try:
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "Testing to see if I can log here. If you see this it worked!"
+            name = "Testing logging."
+            embed.set_author(name=name, icon_url="https://i.imgur.com/z4qtYxT.png")
+            embed.set_footer(text = "This test message will self delete in 30 seconds")
+            test = await self.bot.send_message(destination = channel, embed = embed)
+
+            #Passed
+            self.settings[server.id]["greetlogchid"] = channel.id
+            self.save()
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "{} has been saved as the channel greeter will log to.".format(channel.mention)
+            name = "The log was succesfully set."
+            embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+            await self.bot.say(embed = embed)
+            await asyncio.sleep(30)
+            await self.bot.delete_message(test)
+
+        except discord.Forbidden:
+            embed = discord.Embed()
+            embed.colour = discord.Colour.red()
+            embed.description = "I do have permission to send messages to that channel (server={}, channel={})".format(server.id, channel.id)
+            embed.set_thumbnail(url="https://i.imgur.com/zNU3Y9m.png")
+            name = "The logging channel was not set!"
+            embed.set_author(name=name, icon_url="https://i.imgur.com/zNU3Y9m.png")
+            await self.bot.say(embed = embed)
+
+    @greetset.command(name="greetchannel", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _greetchannel_(self, ctx, channel: discord.Channel = None):
+        """Sets the channel greeters have to greet from, a channel tha `[p]greet` is locked to. Mention a channel with this command, or use it's ID to set it.\ni.e: [p]greetset greetchannel #greeter-logs\nIf no channel is given, the command is unlocked for use anywhere on the server."""
+
+        server = ctx.message.server
+        if channel is None:
+            self.settings[server.id]["greetchannelid"] = None
+            self.save()
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "The greeter channel has been cleared. The greet command can be used anywhere on the server."
+            name = "The greeter channel was succesfully cleared."
+            embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+            await self.bot.say(embed = embed)
+            return
+        
+        try:
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "Testing to see if I can post here. If you see this it worked!"
+            name = "Testing greet channel."
+            embed.set_author(name=name, icon_url="https://i.imgur.com/z4qtYxT.png")
+            embed.set_footer(text = "This test message will self delete in 30 seconds")
+            test = await self.bot.send_message(destination = channel, embed = embed)
+
+            #Passed
+            self.settings[server.id]["greetchannelid"] = channel.id
+            self.save()
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "{} has been saved as the greeter channel.".format(channel.mention)
+            name = "The greeter channel was succesfully set."
+            embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+            await self.bot.say(embed = embed)
+            await asyncio.sleep(30)
+            await self.bot.delete_message(test)
+
+        except discord.Forbidden:
+            embed = discord.Embed()
+            embed.colour = discord.Colour.red()
+            embed.description = "I do have permission to send messages to that channel (server={}, channel={})".format(server.id, channel.id)
+            embed.set_thumbnail(url="https://i.imgur.com/zNU3Y9m.png")
+            name = "The greeter channel was not set!"
+            embed.set_author(name=name, icon_url="https://i.imgur.com/zNU3Y9m.png")
+            await self.bot.say(embed = embed)
+
+    @greetset.command(name="greeter", pass_context=True, no_pm=True)
+    @checks.serverowner()
+    async def _greeter_(self, ctx, switch):
+        """Enable or disable the greeter cog for the server.\n [p]greetset swtich [enabled/disable]"""
+
+        server = ctx.message.server
+        onemoji = discord.utils.get(self.bot.get_all_emojis(), id="330419505589256192").url
+        offemoji = discord.utils.get(self.bot.get_all_emojis(), id="330419505563959296").url
+        embed = discord.Embed()
+        embed.colour = discord.Colour(0xEA2011)
+
+        if switch == "enable":
+            enabled = True
+            embed.description = "The greeter cog has been enabled for this server."
+            embed.set_author(name="Greeter has been toggled on.", icon_url=onemoji)
+            embed.colour = discord.Colour.green()
+        
+        elif switch == "disable":
+            enabled = False
+            embed.description = "The greeter cog has been disabled for this server."
+            embed.set_author(name="Greeter has been toggled off.", icon_url=offemoji)
+            embed.colour = discord.Colour(0xEA2011)
+
+        self.settings[server.id]["enabled"] = enabled
+        self.save()
+
+        await self.bot.say(embed = embed)
+    
+    def _hasrole_(self, user, role):
+        # Check if the given user has a role.
+
+        for r in user.roles:
+            if r is role:
+                return True
+        return False
+
+
+    async def _greetlogger_(self, status, timestamp, server, greetuser = None, newuser = None, mrole = None):
+        # Logging for the greeter cog.
         logch = discord.utils.get(server.channels, id=self.settings[server.id].get("greetlogchid"))
 
+        if status == "Success":
 
+            embed = discord.Embed()
+            embed.colour = discord.Colour.green()
+            embed.description = "{} was given the {} role by {}\n\n {}'s ID: `{}`".format(newuser.mention, mrole.mention, greetuser.mention, newuser.name, newuser.id)
+            embed.timestamp = timestamp
+            name = "{} greeted {}".format(greetuser.name, newuser.name)
+            embed.set_author(name=name, icon_url="https://i.imgur.com/Kw0C9gK.png")
+            embed.set_thumbnail(url = newuser.avatar_url or discord.Embed.Empty)
+            embed.set_footer(text = "Greeter {}'s ID: `{}`".format(greetuser.name, greetuser.id))
 
-        if status != "Success":
-            embedmsg = discord.Embed(title="{} greeted {} to the server\n\n",
-                                         colour=discord.Colour(0x54d824),
-                                         description="This server is currently on `{}` of setup".format("test"),
-                                         timestamp = timestamp)
+            try:
+                await self.bot.send_message(destination = logch, embed = embed)
+            except discord.Forbidden:
+                self.bot.logger.warning(
+                    "Did not have permission to send message to logging channel (server={}, channel={})".format(logch.server.id, logch.id)
+                )
+        
+        elif status == "Forbidden":
+
+            embed = discord.Embed()
+            embed.colour = discord.Colour(0xEA2011)
+            embed.description = "I failed to give {} the {} role as requested by {}. Do I still have the mangage_roles perm, and is it under my highest role?\n\n {}'s ID: `{}`".format(newuser.mention, mrole.mention, greetuser.mention, newuser.name, newuser.id)
+            embed.timestamp = timestamp
+            embed.set_thumbnail(url="https://i.imgur.com/zNU3Y9m.png")
+            name = "{} attempted to welcome {}.".format(greetuser.name, newuser.name)
+            embed.set_author(name=name, icon_url="https://i.imgur.com/zNU3Y9m.png")
+            embed.set_footer(text = "Greeter {}'s ID: `{}`".format(greetuser.name, greetuser.id))
+
+            try:
+                await self.bot.send_message(destination = logch, embed = embed)
+            except discord.Forbidden:
+                self.bot.logger.warning(
+                    "Did not have permission to send message to logging channel (server={}, channel={})".format(logch.server.id, logch.id)
+                )
 
     def save(self):
-        setpath = os.path.join('data', 'greet', 'settings.json')
+        setpath = os.path.join('data', 'greeter', 'settings.json')
         dataIO.save_json(setpath, self.settings)
 
 
 def check_folder():
-    path = os.path.join('data', 'greet')
+    path = os.path.join('data', 'greeter')
     if not os.path.exists(path):
         print('Creating ' + path + '...')
         os.makedirs(path)
@@ -88,7 +317,7 @@ def check_files():
     files = {
         "settings.json": {}
     }
-    datapath = os.path.join('data', 'greet')
+    datapath = os.path.join('data', 'greeter')
     for filename, value in files.items():
         path = os.path.join(datapath, filename)
         if not os.path.isfile(path):
--- test/test.py
+++ test/test.py
@@ -35,13 +35,13 @@ class test:
         timestamp = datetime.datetime.today()
         auth = ctx.message.author
 
-        embedmsg = discord.Embed(#title="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name),
-                                        colour=discord.Colour(0x54d824),
+        embedmsg = discord.Embed(#title="{} greeted {}\n\n".format(greetuser.mention, newuser.mention),
+                                        colour=discord.Colour.green(),
                                         description="{} was given the {} role".format(ctx.message.author.name, ctx.message.author.top_role),
                                         timestamp = timestamp)
         embedmsg.set_author(name="{} greeted {} to the server\n\n".format(ctx.message.author.mention, ctx.message.author.name,
                             icon_url=auth.avatar_url))
-        embedmsg.set_footer(text = "{}'s (Greeter) ID: `{}` | {}'s (New member) ID `{}`".format(auth.name, auth.id, auth.mention, auth.id))
+        embedmsg.set_footer(text = "Greeter {}'s ID: `{}` | New member {}'s ID `{}`".format(auth.name, auth.id, auth.name, auth.id))
 
         await self.bot.say(embed=embedmsg)
 
@@ -58,6 +58,13 @@ class test:
                                         timestamp = timestamp)
 
         await self.bot.say(embed=embedmsg)
+
+    def embed_set_author_name(embed: discord.Embed, channel_name: str, member_name: str, system_name: str, avatar_url: str):
+    name = "#{}: {}".format(channel_name, member_name)
+    if system_name:
+        name += " ({})".format(system_name)
+
+    embed.set_author(name=name, icon_url=avatar_url or discord.Embed.Empty)
 
 
 
